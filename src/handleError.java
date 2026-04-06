5b6f9c0470c8954bf4d0f5d50faa76136848f7a5
Luiz <lamasluizmiguel@gmail.com>
2026-04-06 16:29:56 -0300

    Correção de alguns erros
    NextTask e TaskHandler

--- src/handleError.java
+++
@@ -1,9 +0,0 @@
-public static void handleError (Task task) {
-    int maxRetries = 3;
-
-    if (task.getAttempts() < maxRetries) {
-        updateTaskStatus(task.getId(), "pending");
-    } else {
-        updateTaskStatus(task.getId(), "error");
-    }
-}
